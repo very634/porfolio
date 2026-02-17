00c931ea4e637c250d285cedc57077b4bce2229c
very634 <mouhv166@gmail.com>
2026-02-17 22:09:08 +0000

    Add files via upload

---
+++ cv.py
@@ -0,0 +1,40 @@
+import streamlit as st
+
+st.title("Technicien Supérieur en géàmatique")
+st.write("passionné de géomatique par sa capacité à modéliser et analyser le territoire.J'utilise en tant que technicien supérieur en géomatique les SIG,la télédétection,et des bases de données pour cartographier et amener des solutions dans l'amenagement térritorial")
+
+
+
+
+with st.sidebar:
+    st.title("Mouhamed BA")
+    st.image("moi.jpeg")
+    st.write("mail: mouhv166@gmail.com")
+    st.write("numéro: +221 70 114 55 89")
+    st.write("Addresse: Lac Rose, Rufisque, SENEGAL")
+    
+
+
+
+st.title("EXPERIENCES PROFESSIONELLES")
+st.write("*collecte et acquition des données")
+st.write("traitement et intégration dans un SIG")
+st.write("")
+st.write("")
+
+st.title("COMPETENCES")
+st.write("*maitrise parfaite de l'outil informatique")
+st.write("*mise en place d'une base de donnée")
+st.write("*Python(Notebook,Jupyter)")
+st.write("*cartographie(maitrise en collecte des données puis le traitement de ses données sur ARCgis)")
+st.write("*capable de réaliser des plans architecturals")
+st.write("*Réalisation des cartes sur ARCgis et Qgis")
+st.write("")
+
+
+
+st.title("FORMATIONS")
+st.write("Bevet de Technicien Supérieur(BTS)")
+st.write("Bac")
+
+
